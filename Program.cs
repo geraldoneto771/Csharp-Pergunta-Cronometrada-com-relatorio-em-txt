45c807cb04a1b18a6d642656d7b8ea706a19a155
geraldoneto771 <geraldopireslacerda771@gmail.com>
2022-09-26 22:24:07 -0300

    Calculadora funcionando com o Timer para resposta e salvando resultado em arquivo txt - possui alguns limitacoes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 ﻿using System;
-
+using System.IO;
+using System.Threading;
+using System.Timers;
 
 namespace PerguntaCronometrada
 {
     class Program
     {
+        private static System.Timers.Timer aTimer;
+        static int num1, num2, respostaCorreta, currentTime = 10;
+        static int respostaUser = 0;
+        static int tempo = 0;
+        static int contador = 20;
+        static string textoResultado;
         static void Main(string[] args)
         {
             Menu();
@@ -12,7 +20,7 @@ namespace PerguntaCronometrada
 
         static void Menu()
         {
-
+            contador = 20;
             Console.Clear();
             Console.WriteLine("---Pergunta Cronometrada---");
             Console.WriteLine("---------------------------");
@@ -37,24 +45,143 @@ namespace PerguntaCronometrada
 
         static void Soma()
         {
-            Console.Clear();
-            Random random = new Random();
-            int num1 = random.Next(1, 100);
-            int num2 = random.Next(1, 100);
-            int respostaCorreta = (num1 + num2);
-            int respostaUser;
+            contador = 20;
 
+            Random random = new Random();
+            num1 = random.Next(1, 100);
+            num2 = random.Next(1, 100);
+            respostaCorreta = (num1 + num2);
+
+            SetTimer();
+            Console.Clear();
+            while ((currentTime > 0))
+            {
+                //Console.Clear();
+                currentTime--;
+                string pergunta = ($"Quanto custa {num1} + {num2}?");
+                //entrando com os dados para ser salvo no arquivo
+
+                Editar(pergunta);
+                aTimer.Dispose();
+                aTimer.Stop();
+
+                break;
+                Console.Clear();
+
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado...");
+
+
+            if (currentTime <= 0)
+            {
+                Console.WriteLine("Infelizmente você não respondeu a tempo");
+            }
 
             do
             {
-                Console.WriteLine($"Quanto custa {num1} + {num2}?");
-                respostaUser = int.Parse(Console.ReadLine());
-            }
-            while (respostaUser != respostaCorreta);
 
-            Console.WriteLine($"Parabéns {num1} + {num2} é: {respostaCorreta}!");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
         }
 
+        static void Editar(string pergunta)
+        {
+            Console.Clear();
+            Console.WriteLine(pergunta);
+            Console.WriteLine("Digite seu texto abaixo (Aperte A para sair)");
+            Console.WriteLine("---------------------------------------");
+
+            string text = "";
+
+            do
+            {
+
+                //Faça enquanto esc não for precionado
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            } while (Console.ReadKey().Key != ConsoleKey.A);
+
+
+            Salvar(text);
+
+        }
+
+        static void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo?");
+            Console.WriteLine("---------------------------------------");
+            // Path - Caminho
+            var path = Console.ReadLine();
+            //criando um arquivo e salvando a string dentro
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Cronometro(int num1, int num2)
+        {
+            int currentTime = 10;
+            int respostaUser;
+            while (currentTime > 0)
+            {
+                Console.Clear();
+                Console.WriteLine($"Quanto custa {num1} + {num2}?");
+                respostaUser = int.Parse(Console.ReadLine());
+                currentTime--;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+
+            }
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado...");
+            Thread.Sleep(2500);
+
+            if (currentTime <= 0)
+            {
+                Console.WriteLine("Infelizmente você não respondeu a tempo");
+            }
+
+
+        }
+
+        private static void SetTimer()
+        {
+
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(1000);
+
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+
+
+
+        }
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+
+            aTimer.Enabled = true;
+            contador--;
+            if (contador == 0)
+            {
+                Console.WriteLine("Infelizmente seu tempo acabou");
+                aTimer.Enabled = false;
+                Thread.Sleep(5000);
+
+                Menu();
+
+            }
+
+        }
 
     }
 }
